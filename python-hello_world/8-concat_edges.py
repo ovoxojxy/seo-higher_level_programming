712907c293e14fb6b76253e0e8dca4fb7d07b6b8
ovoxojxy <jjarod517@gmail.com>
2023-05-05 20:16:21 -0700

    hello world

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-         language that combines remarkable power with very clear syntax"
-print(str)
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str.split()
+str = " ".join(str[5:8])
+print(str + ' with Python')
