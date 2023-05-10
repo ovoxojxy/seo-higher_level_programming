f94831a7fc7d635bd3675fede07bfc894fb9e493
ovoxojxy <jjarod517@gmail.com>
2023-05-10 16:01:30 -0700

    alphabet

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range (1,101):
         if (i % 5 == 0 and i % 3 != 0):
             print ('Buzz', end = ' ')
-        elif (i % 3 == 0 i % 5 != 0):
+        elif (i % 3 == 0 and i % 5 != 0):
             print ('Fizz', end = ' ')
         elif (i % 3 == 0 and i % 5 == 0):
             print('Fizzbuzz')
