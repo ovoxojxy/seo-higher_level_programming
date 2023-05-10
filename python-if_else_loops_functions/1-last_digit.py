634be67670be657f09e58631bc52782fce0ee695
ovoxojxy <jjarod517@gmail.com>
2023-05-10 09:36:04 -0700

    last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ elif(x >= 0):
     y = x%10
 if(y > 5):
     print(f"Last digit of {x} is {y} and is greater than 5")
-elif(y < 6 and numcon != 0):                                         
+elif(y < 6 and y != 0):                                         
     print(f"Last digit of {x} is {y} and is less than 6 and not 0")
 elif(y ==0):
     print(f"Last digit of {x} is {y} and is 0")
