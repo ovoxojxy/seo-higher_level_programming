b40ff0b937b6bd637f21c6c9605415282877b20a
ovoxojxy <jjarod517@gmail.com>
2023-05-10 20:40:11 -0700

    alphabet

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range (0, 100):
+for i in range(0, 100):
     num = str(i).zfill(2)
-    print("{0}".format(num), end = ', ' if i < 99 else "\n")
+    print("{0}".format(num), end=', ' if i < 99 else "\n")
