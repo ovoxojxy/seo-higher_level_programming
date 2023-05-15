e93528183a9082fe484ffccad600858eaa8edf8c
ovoxojxy <jjarod517@gmail.com>
2023-05-15 12:02:20 -0700

    new

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):                             
+    x = -1                                                               
+    for i in range (len(my_list)):                                               print(my_list[x])                                                
+        x -= 1 
