3dd6369f36ad6eaa39aca90ac2f96af956024c66
ovoxojxy <jjarod517@gmail.com>
2023-05-18 12:02:01 -0700

    new

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     m = len(matrix)
-    x = '\n'.join([.join(['{:m}'.format(item) for item in row]) for row in matrix])
+    x = '\n'.join([.join(['{:d}'.format(item) for item in row]) for row in matrix])
     return x
