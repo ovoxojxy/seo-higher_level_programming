d14a111fd49c7ff18a0fd05f517066eccead4307
ovoxojxy <jjarod517@gmail.com>
2023-05-18 11:20:59 -0700

    new

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) < 1:
+        length = len(sentence)
+        first = None
+    else:
+        length = len(sentence)
+        first = sentence[0]
+    return x, y
