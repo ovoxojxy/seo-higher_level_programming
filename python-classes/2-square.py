6f6b511e59a7e948232374b0732bc5c28aa0c396
ovoxojxy <jjarod517@gmail.com>
2023-05-24 17:57:35 -0700

    square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-'''This module is here to create a class that defines a square with a private instance attribute and instantiation'''
+''' This module is to create a class that defines a square''' 
 
 
 class Square:
     '''This class defines a sqaure and verifies the type of the size variable and it being greater than 0'''
-
     def __init__(size, size=0):
         ''' This constructor initializes the class'''
         self._Square__size = size
