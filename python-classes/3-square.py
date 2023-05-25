4f2880cb790d6604504360c9307593d9b55a1116
ovoxojxy <jjarod517@gmail.com>
2023-05-24 18:03:22 -0700

    square

---
+++ python-classes/3-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' This module creates a class Square that defines a square by: (based on 2-square.py) '''
+
+
+class Square:
+    ''' This class defines a square '''
+    def __init__(self, size=0):
+        ''' This method serves as a constructor '''
+        self._Square__size = size
+
