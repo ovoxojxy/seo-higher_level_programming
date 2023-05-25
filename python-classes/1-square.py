6a3aeeef4886886f9888588c99d0186701522b67
ovoxojxy <jjarod517@gmail.com>
2023-05-24 17:26:27 -0700

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,5 +5,5 @@
 class Square:
     '''This class defines a square'''
 
-    def __init__(self, _Square__size):
-        self.size = size
+    def __init__(self, size):
+        self.size = _Square__size
