12b6affc03ccfc9d727a6e9ce2264e5430c5a751
ovoxojxy <jjarod517@gmail.com>
2023-05-24 17:07:08 -0700

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+'''This module is created in order to create the first empty class'''
 class Square:
+    ''' This empty class called square is used to define a square'''
     pass
