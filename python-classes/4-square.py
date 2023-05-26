b93501cf91cd9d24eb997368d4a264251fdc8a39
ovoxojxy <jjarod517@gmail.com>
2023-05-26 13:26:04 -0700

    check doc

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,11 +1,5 @@
-#!/usr/bin/python3                                                       
-''' This module creates a class Square that defines a square by taking'''       
-
-
+#!/usr/bin/python3
+    ''' This module defines sqaures and blah blah blah'''
 class Square:
-    def __init__(self, size=0):
-    ''' This method serves as a constructor '''
-        self._Square__size = size
-
-    def area(self):
-        return self._Square__size ** 2 
+    ''' Documentation test chekc for class'''
+    pass
