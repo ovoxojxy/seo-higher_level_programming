2a8dfa4488f97ab93be33c3826297892462e4ce6
ovoxojxy <jjarod517@gmail.com>
2023-05-26 13:59:18 -0700

    doc

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 ''' This module defines sqaures and blah blah blah'''
+
+
 class Square:
     ''' Documentation test chekc for class'''
     def __init__(self, size=0):
         self.size = size
     def area(self):
         return self.size ** 2
-
