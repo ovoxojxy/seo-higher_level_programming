d779ad61ff6a829a8f5ee573b4b316f6bc2b6c99
ovoxojxy <jjarod517@gmail.com>
2023-05-26 16:23:37 -0700

    try & except

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,5 +6,17 @@ class Square:
     '''This class defines a sqaure and verifies the type of the size variable and it being greater than 0'''
     def __init__(self, size=0):
         ''' This constructor initializes the class'''
-        self._Square__size = size
+        if size => 0 and type(size) is int:
+            self._Square__size = size
+        elif size < 0:
+           try:
+               print(x)
+            except ValueError:
+                print('size must be >= 0')
+        elif type(size) != int:
+            try:
+                print(x)
+            except TypeError:
+                print('size must be an integer')
+
 
