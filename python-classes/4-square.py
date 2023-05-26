356309eeb269d10fb3971c53b002afb57705d52d
ovoxojxy <jjarod517@gmail.com>
2023-05-26 13:28:10 -0700

    doc

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-    ''' This module defines sqaures and blah blah blah'''
+''' This module defines sqaures and blah blah blah'''
 class Square:
     ''' Documentation test chekc for class'''
     pass
