2bed9593f72d92cb072fa5d90e072accc1cd1e12
ovoxojxy <jjarod517@gmail.com>
2023-05-26 13:55:00 -0700

    doc

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,12 +6,6 @@ class Square:
         self.size = size
     def area(self):
         return self.size ** 2
-
-try:
-    my_square = Square(89)
-    print(my_square.size)
-    my_square.size = '89'
-    print(my_sqaure.size)
-except Exception as e:
-    print('size must be an integer')
+        except Exception as e:
+            print('size must be an integer')
 
