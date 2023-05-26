0314dae501acb50cec6c1458cd3177e8a7d23f92
ovoxojxy <jjarod517@gmail.com>
2023-05-25 21:05:40 -0700

    private

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,14 +3,11 @@
 
 
 class Square:
-    ''' This class ishere to define a sqaure'''
-    def __init__(self, size=0):
-        self.__Square_size = size
-
-    def size(self, value):
-        self.value = value
-
-    def area(self, size):
-        return self.size ** 2
+''' This class defines a square '''                                  
+    def __init__(self, size=0):                                          
+    ''' This method serves as a constructor '''                      
+        self._Square__size = size                                        
+                                                                             def area(self):                                                      
+        return self._Square__size** 2   
 
 
