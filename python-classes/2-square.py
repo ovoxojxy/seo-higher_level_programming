682f12ed615299d9ad6b448ea7691a15a4714bb3
ovoxojxy <jjarod517@gmail.com>
2023-05-26 16:05:48 -0700

    square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-    ''' This module creates a class Square that defines a square by'''
+''' No idea why this test can't read this documentation'''
 
 
 class Square:
     '''This class defines a sqaure and verifies the type of the size variable and it being greater than 0'''
-    def __init__(size, size=0):
+    def __init__(self, size=0):
         ''' This constructor initializes the class'''
         self._Square__size = size
 
