e5295ed3421e88caa12e9ada99960721918317ee
ovoxojxy <jjarod517@gmail.com>
2023-05-26 20:02:38 -0700

    set and get

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,3 +9,10 @@ class Square:
 
     def area(self):
         return self.size ** 2
+    
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__ = value
---
+++ python-classes/6-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' this module serves to define a class that defines a square'''
+
+
+class Sqaure:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+
+    def size(self, value):
+        return self.size
+        
+
+
+
+
+
