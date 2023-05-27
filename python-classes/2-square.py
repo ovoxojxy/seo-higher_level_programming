7a2a7e1f9b1224cf5a8d9795fd7454743bc778f1
ovoxojxy <jjarod517@gmail.com>
2023-05-26 17:23:33 -0700

    square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' This module's purpose is to define a class that defines a square and validates it's size with TypeError and ValueError functions'''
+''' This module creates a class Square that defines a square by''' 
 
 
 class Square:
