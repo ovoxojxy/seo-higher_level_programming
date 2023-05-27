3b04ad1f1760563634e4f8fc6d533e29d5f23d34
ovoxojxy <jjarod517@gmail.com>
2023-05-26 20:14:19 -0700

    booo

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,7 +7,33 @@ class Sqaure:
         self.size = size
 
     def size(self, value):
-        return self.size
+        return self.__size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size mustt be >= 0')
+        self.__size = value
+
+    def position(self, value):
+        if not instance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(num, int) for num in value) or \
+                not att(num >= 0 for num in value):
+                    raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+            return
+
+        for _ in range(slef.position[1]):
+            print()
+
+        for _ in range(self.size):
+            print(" " * self.position[0] + '#' * self.size)
         
 
 
