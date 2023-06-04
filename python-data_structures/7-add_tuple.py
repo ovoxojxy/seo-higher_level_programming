9c0fa30e9caf09ace3867c0b9e1555a20a6c8c6e
ovoxojxy <jjarod517@gmail.com>
2023-06-04 11:54:10 -0700

    fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -9,20 +9,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             for i in range(len(list_a)):
                 tup = (tuple_a[0] +tuple_b[0], tuple_a[1] + tuple_b[1])
-        elif (len(list_a) > len(list_b)):
-            for i in range(len(list_a) - len(list_b)):
-                list_b.append(0)
-            for i in range(len(list_a)):
-                new_list.append(list_a[i] + list_b[i])
-            tup = tuple(new_list)
-        elif (len(list_a) < len(list_b)):
-            for i in range(len(list_b) - len(list_a)):
-                list_a.append(0)
-            for i in range(len(list_a)):
-                new_list.append(list_a[i] + list_b[i])
-            tup = tuple(new_list)
-        elif len(list_a) and len(list_b) == 0:
-            new_list.append(0)
-            new_list.append(0)
-            tup = tuple(new_list)
-        return tup
+    elif (len(list_a) > len(list_b)):
+        for i in range(len(list_a) - len(list_b)):
+            list_b.append(0)
+        for i in range(len(list_a)):
+            new_list.append(list_a[i] + list_b[i])
+        tup = tuple(new_list)
+    elif (len(list_a) < len(list_b)):
+        for i in range(len(list_b) - len(list_a)):
+            list_a.append(0)
+        for i in range(len(list_a)):
+            new_list.append(list_a[i] + list_b[i])
+        tup = tuple(new_list)
+    elif len(list_a) and len(list_b) == 0:
+        new_list.append(0)
+        new_list.append(0)
+        tup = tuple(new_list)
+    return tup
