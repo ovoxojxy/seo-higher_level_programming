00c3bab746701f61c3bf14d7a382fe1cf30b6f64
ovoxojxy <jjarod517@gmail.com>
2023-06-04 11:52:05 -0700

    fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        new_tuple = (tuple_a[0] +tuple_b[0], tuple_a[1] + tuple_b[1])
-        return new_tuple
-    elif len(tuple_b) <= 2 or len(tuple_a) <= 2:
-        if len(tuple_a) < len(tuple_b):
-            tuple_b = (tuple_b[0], 0)
-            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-            return new_tuple
-        elif len(tuple_b) < len(tuple_a):
-            tuple_b = (tuple_b[0], 0)
-            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-            return new_tuple
-    elif len(tuple_b) <= 2 and len(tuple_a) <= 2:
-            tuple_b = (tuple_b[0], 0)
-            tuple_a = (tuple_a[0], 0)
-            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-            return new_tuple
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+    new_list = []
+    if len(list_a) == len(list_b):
+        if len(tuple_a) == 0 and len(tuple_b) == 0:
+            tup = (0, 0)
+        else:
+            for i in range(len(list_a)):
+                tup = (tuple_a[0] +tuple_b[0], tuple_a[1] + tuple_b[1])
+        elif (len(list_a) > len(list_b)):
+            for i in range(len(list_a) - len(list_b)):
+                list_b.append(0)
+            for i in range(len(list_a)):
+                new_list.append(list_a[i] + list_b[i])
+            tup = tuple(new_list)
+        elif (len(list_a) < len(list_b)):
+            for i in range(len(list_b) - len(list_a)):
+                list_a.append(0)
+            for i in range(len(list_a)):
+                new_list.append(list_a[i] + list_b[i])
+            tup = tuple(new_list)
+        elif len(list_a) and len(list_b) == 0:
+            new_list.append(0)
+            new_list.append(0)
+            tup = tuple(new_list)
+        return tup
