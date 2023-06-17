94beb749a43437b9c15008d2530fac841e8c8e0d
ovoxojxy <jjarod517@gmail.com>
2023-06-17 06:50:05 -0700

    doc

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''This module writes a script that lists all states frm the database hbt                                                                          
-n_e_0_usa using 3 arguments and sorting in ascending order by states '''
+#This module writes a script that lists all states frm the database hbt                                                                          
+#n_e_0_usa using 3 arguments and sorting in ascending order by states '''
 
 
 import sys
@@ -9,29 +9,29 @@ import MySQLdb
 
 
 
-'''get commandd line arguemtns'''
+#get commandd line arguemtns'''
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
-''' establish database connection'''
+#establish database connection'''
 cnx = MySQLdb.connect(user=username, passwd=password,
 host='localhost', port=3306, db=database)
 cursor = cnx.cursor()
 
 
-'''execute the query to fetch all cities'''
+#execute the query to fetch all cities'''
 query = 'SELECT * FROM states ORDER BY id ASC'
 cursor.execute(query)
 
-'''fetch all the rows'''
+#fetch all the rows'''
 rows = cursor.fetchall()
 
-'''iterate over the rows and print city names'''
+#iterate over the rows and print city names'''
 for row in rows:
     state_id, state_name = row
     print(state_name)
 
-'''close cursor and database connection'''
+#close cursor and database connection'''
 cursor.close()
 cnx.close()
