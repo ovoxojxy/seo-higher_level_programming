51c3124c27cb3ca0bdf7ff861ad1914e06f609da
ovoxojxy <jjarod517@gmail.com>
2023-07-27 06:52:16 -0700

    tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -41,4 +41,40 @@ class testBase(unittest.TestCase):
         self.assertEqual(str(e.exception), msg)
 
 
-
+    def test_D_constructor_args_2(self):
+        with self.assertRaises(TypeError) as e:
+            Base.__init__(self, 1, 2)
+        msg = "__init__() takes from 1 to 2 positional arguments but 3 \
+        were given"
+        self.assertEqual(str(e.exception), msg)
+                
+    def test_E_consecutive_ids(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id + 1, b2.id)
+                    
+    def test_F_id_synced(self):
+        b = Base()
+        self.assertEqual(getattr(Base, "_Base__nb_objects"), b.id)
+    
+    def test_F_id_synced_more(self):
+        b = Base()
+        b = Base("Foo")
+        b = Base(98)
+        b = Base()
+        self.assertEqual(getattr(Base, "_Base__nb_objects"), b.id)
+                            
+    def test_G_custom_id_int(self):
+        i = 98
+        b = Base(i)
+        self.assertEqual(b.id, i)
+                                
+    def test_G_custom_id_str(self):
+        i = "FooBar"
+        b = Base(i)
+        self.assertEqual(b.id, i)
+    
+    def test_G_id_keyword(self):
+        i = 421
+        b = Base(id=i)
+        self.assertEqual(b.id, i)
