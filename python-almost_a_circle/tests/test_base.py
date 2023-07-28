ef2de086ab2bdf71567377cd0ecf385bf2625baf
ovoxojxy <jjarod517@gmail.com>
2023-07-28 07:59:48 -0700

    tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -3,33 +3,42 @@
 import unittest
 import Base
 
-class testBase(unittest.TestCase):
+class TestBase(unittest.TestCase):
+    '''Tests the Base class.'''
 
-    def test__init__(self):
-        b = base.Base()
-        self.asserIsNotNone(b)
-
-    def test_Base(self):
-        id = Base.Base()
-        self.assertTrue(id
-
-    def setup(self):
-        Base._Base__nb_objects = 0
+    def setUp(self):
+        '''Imports module, instantiates class'''
+        base.Base._Base__nb_objects = 0
         pass
 
     def tearDown(self):
+        '''Cleans up after each test_method.'''
         pass
-    
-    def test_nb__objects_initialized(self):
-        self.assertTrue(hasattr(Base.Base, "_Base__nb_objects"))
 
-    def test_nb_tests(self):
-        self.assertEqual(type(getattr(Base, "_Base__nb_objects")), int)
+    def test_A_nb_objects_private(self):
+        '''Tests if nb_objects is private class attribute.'''
+        self.assertTrue(hasattr(base.Base, "_Base__nb_objects"))
 
-    def test_instantiation(self):
-        b = Base.Base()
-        self.assertEqual(str(type(b)), "<class 'models.base.Base'>")
+    def test_B_nb_objects_initialized(self):
+        '''Tests if nb_objects initializes to zero.'''
+        self.assertEqual(getattr(base.Base, "_Base__nb_objects"), 0)
+        
+    def test_C_instantiation(self):
+        '''Tests Base() instantiation.'''
+        b = base.Base()
+        self.assertEqual(str(type(b)), "<class 'base.Base'>")
+        self.assertEqual(b.__dict__, {"id": 1})
+        self.assertEqual(b.id, 1)
+    def test_D_constructor(self):
+        '''Tests constructor signature.'''
+        with self.assertRaises(TypeError) as e:
+            base.Base.__init__()
+        msg = "Base.__init__() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
 
-if __name__ == "__main__":
-    unittest.main()
-
+    def test_D_constructor_args_2(self):
+        '''Tests constructor signature with 2 notself args.'''
+        with self.assertRaises(TypeError) as e:
+            base.Base.__init__(self, 1, 2)
+            msg = "Base.__init__() takes from 1 to 2 positional arguments but 3 \ were given"
+            self.assertEqual(str(e.exception), msg)
